082e966bb7ae355af3b18c2c06fdf72b109641b6
Sujithagithub <139635410+Sujithagithub@users.noreply.github.com>
2023-07-15 20:08:38 +0530

    Add files via upload
    Day 1 classwork 2

---
+++ day2/New.java
@@ -0,0 +1,20 @@
+package week1.day2;
+
+import week1.day1.Maths;
+
+public class New {
+
+	public static void main(String[] args) {
+		
+Maths ms=new Maths();
+System.out.println(ms.add1(9, 10));
+int add=ms.add1(9, 10);
+System.out.println(add);
+System.out.println(ms.multiply(9, 10));
+System.out.println(ms.div(9, 10));
+
+
+
+	}
+
+}
