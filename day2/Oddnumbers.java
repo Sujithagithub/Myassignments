a2b56b97f03948965c7e19fa12cedd3ffe884b4f
Sujithagithub <139635410+Sujithagithub@users.noreply.github.com>
2023-07-17 11:54:49 +0530

    Add files via upload

---
+++ day2/Oddnumbers.java
@@ -0,0 +1,17 @@
+package week1.day2;
+
+public class Oddnumbers {
+
+	public static void main(String[] args) {
+		int num=20;
+		for (int i = 0; i<num ; i++) {
+			if(i%2==1)
+			{
+				System.out.println("Odd Numbers are:"+i);
+			}
+			
+		}
+
+	}
+
+}
